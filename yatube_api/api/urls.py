54681e169a28cd12972eaadba4f6ad7873c0a320
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-10-17 00:57:31 +0500

    viewset PostViewSet modified:(api/views.py) * add: (yatube_api/api/permissions.py)   modified: (yatube_api/yatube_api/settings.py) разграничение прав   доступа для пользователей * modified: (yatube_api/api/urls.py) эндпоинты постов

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import include, path
 
+from rest_framework import routers
+
+from api.views import PostViewSet
+
+
+router = routers.DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
+
 urlpatterns = [
+    path('v1/', include(router.urls)),
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,15 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+
+from posts.models import Post
+
+from api.permissions import AuthorOrReadOnly
+from api.serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
